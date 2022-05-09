849733a5c83f8597c0e73caeaa95e4b8c34e9659
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2022-05-10 00:46:23 +0300

    add useRoveFocus

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 
 import { DropdownItem } from './DropdownItem';
-import { dropdownListData } from './helper';
+import { dropdownListData, useRoveFocus } from './helper';
 
 import './App.css';
 
 const App = () => {
   const [isOpenList, setOpenList] = useState(false);
   const handleOpen = () => setOpenList(!isOpenList);
+  const [focus, setFocus] = useRoveFocus(dropdownListData.length);
 
   return (
     <ul className="dropdown-list" onClick={handleOpen}>
@@ -18,6 +19,9 @@ const App = () => {
           isOpenList={isOpenList}
           dropdownData={dropdownListData}
           index={index}
+          focus={focus === index}
+          setFocus={setFocus}
+          handleOpen={handleOpen}
         />
       )}
     </ul>
--- src/DropdownItem.js
+++ src/DropdownItem.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-export const DropdownItem = ({ item, isOpenList, dropdownData, index }) => {
+export const DropdownItem = ({ item, isOpenList, dropdownData, index, focus, setFocus, handleOpen }) => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if (focus) {
+            ref.current.focus();
+        }
+    }, [focus]);
+
     const handleSelect = () => {
         const getSelectedItem = dropdownData.splice(index, 1).toString();
         dropdownData.unshift(getSelectedItem);
     }
 
+    const handleFocus = (event) => {
+        setFocus(index);
+
+        if (event.key === 'Enter') {
+            handleSelect();
+            handleOpen();
+        }
+    };
+
     return (
         <li
             className={`dropdown-item ${isOpenList ? 'dropdown__open' : ''}`}
             onClick={(index !== 0) ? handleSelect : undefined}
+            tabIndex={focus ? 0 : -1}
+            role="button"
+            ref={ref}
+            onKeyPress={e => handleFocus(e)}
         >
             {item}
         </li >
